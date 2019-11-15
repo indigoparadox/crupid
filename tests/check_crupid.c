df98f03468223e5a43360a1d5da64c77c09e8976
indigoparadox <indigo.repo@interfinitydynamics.info>
2019-11-15 15:38:47 -0500

    Added tests and ignore executable.

---
+++ tests/check_crupid.c
@@ -0,0 +1,35 @@
+
+#include <check.h>
+#include "../src/crupid.h"
+
+#include <stdio.h>
+
+START_TEST( test_example ) {
+}
+END_TEST
+
+static void setup_example() {
+}
+
+static void teardown_example() {
+}
+
+Suite* crupid_suite( void ) {
+   Suite* s = NULL;
+   TCase* tc_example = NULL;
+
+   s = suite_create( "crupid" );
+
+   tc_example = tcase_create( "example" );
+
+   tcase_add_checked_fixture(
+      tc_example, setup_example, teardown_example );
+
+   tcase_add_loop_test( tc_example, test_example, 2, 3 );
+   tcase_add_test( tc_example, test_example );
+
+   suite_add_tcase( s, tc_example );
+
+   return s;
+}
+
